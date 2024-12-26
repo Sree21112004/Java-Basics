f08ff0b21594be3060371026996f79fc38468871
Sree Dharma A V <69962045+Sree21112004@users.noreply.github.com>
2024-12-26 18:44:30 +0530

    Add files via upload

---
+++ Pattern_3.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                        Reverse Right half triangle
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int r=sc.nextInt();
+		for(int i=r;i>=1;i--){
+		    for(int j=i;j>=1;j--){
+		        
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+	}
+}
