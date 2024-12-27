eb67e6bf120d7309ae1e1eacc3d0e9dafa9d0f06
Sree Dharma A V <69962045+Sree21112004@users.noreply.github.com>
2024-12-27 17:12:50 +0530

    Add files via upload

---
+++ Pattern_10.java
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+                            Pattern - medium
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int r=sc.nextInt();
+		for(int i=0;i<r;i++){
+		    for(int j=0;j<i;j++){
+		        System.out.print("* ");
+		    }
+		    for(int j=i;j<r;j++){
+		        System.out.print("  ");
+		    }
+		    for(int j=i;j<r;j++){
+		        System.out.print("  ");
+		    }
+		    for(int j=0;j<i;j++){
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+		for(int i=0;i<r;i++){
+		    for(int j=i;j<r;j++){
+		        System.out.print("* ");
+		    }
+		    for(int j=0;j<i;j++){
+		        System.out.print("  ");
+		    }
+		    for(int j=0;j<i;j++){
+		        System.out.print("  ");
+		    }
+		    for(int j=i;j<r;j++){
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+	}
+}
