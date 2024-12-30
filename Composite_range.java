eee76b4b0485aa1d0af712f67b931eccf8652b21
Sree Dharma A V <69962045+Sree21112004@users.noreply.github.com>
2024-12-30 11:32:50 +0530

    Add files via upload

---
+++ Composite_range.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Composite Number Range
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+    	int start=sc.nextInt();
+    	int end=sc.nextInt();
+    	
+    	for(int num=start;num<=end;num++){ 
+    	    int count=0;
+        	for(int i=1;i<=num;i++){
+        	    if(num%i==0){
+        	        count++;
+        	    }
+        	}
+        	if(count>2){
+        	    System.out.println(num);
+        	}
+    	}
+	}
+}
